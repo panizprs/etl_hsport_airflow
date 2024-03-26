dfd21a464a6f712ee1a2e71249cdd92c9d06d303
Paniz <paniz@Panizs-MacBook-Pro.local>
2024-03-26 19:07:40 +0100

    load to sqldb

--- load_data.py
+++ load_data.py
@@ -1,15 +1,17 @@
 import pandas as pd
+import duckdb
 
 input_data_dir = 'input_data/'
 customers = pd.read_excel(f'{input_data_dir}H+ Sport Customers.xlsx', sheet_name='data')
-print(customers.head())
-print(len(customers))
 unique_columns = ['FirstName', 'LastName', 'Email', 'Phone']
 customers = customers.drop_duplicates(unique_columns, keep = 'first')
-print(len(customers))
-print(customers.info())
 
 employees = pd.read_excel(f'{input_data_dir}H+ Sport Employees.xlsx', sheet_name='Employees')
 columns_to_drop = [0.0291]
 employees = employees.drop(columns = columns_to_drop)
 
+with duckdb.connect('transformed_data/hsport.db') as con:
+    con.sql("CREATE TABLE customers AS SELECT * FROM customers")
+    con.sql("CREATE TABLE employees AS SELECT * FROM employees")
+
+    print(con.sql('select * from employees limit 5'))
